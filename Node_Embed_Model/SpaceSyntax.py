fc6ca99d52557d66c86df395353771283ebd556d
18375166@buaa.edu.cn <18375166@buaa.edu.cn>
2025-05-30 16:42:51 +0800

    get spacesytax value

--- Node_Embed_Model/SpaceSyntax.py
+++ Node_Embed_Model/SpaceSyntax.py
@@ -7,17 +7,15 @@ import numpy as np
 
 def get_connectivity(g):
     temp_g = momepy.node_degree(g)
-    print(temp_g.edges(data=True))
+    return temp_g.nodes(data=True)
 
 def get_integration(g):
     temp_g = momepy.closeness_centrality(g)
-    print(temp_g.edges(data=True))
+    return temp_g.nodes(data=True)
 
 def get_choice(g):
     temp_g =  momepy.betweenness_centrality(g)
-    print(temp_g.edges(data=True))
-
-
+    return temp_g.nodes(data=True)
 
 def make_directed_graph(adj_mx):
     """
@@ -30,8 +28,8 @@ def make_directed_graph(adj_mx):
     for i in range(len(temp_list)):
         for j in range(len(temp_list[i])):
             if adj_mx[i][j] != 0:
-                DiG.add_edge(i, j,  key='A', mm_len=adj_mx[i][j])
-
+                DiG.add_edge(i, j, key='A', mm_len=adj_mx[i][j])
+    
     return DiG
 
 def get_space_syntax_embed(adj_mx):
@@ -44,6 +42,6 @@ def get_space_syntax_embed(adj_mx):
     
 
 l = ['CD','BAY','LA','SZ']
-matrix = np.load('raw_data/'+ l[3] +'/matrix.npy')
+matrix = np.load('raw_data/'+ l[0] +'/matrix.npy')
 print(matrix.shape)
 get_space_syntax_embed(matrix)
